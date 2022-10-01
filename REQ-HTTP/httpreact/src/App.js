4d660d2c05f74c04071aa66a6d7016ed479f6afd
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-10-01 20:18:21 -0300

    finished project

--- REQ-HTTP/httpreact/src/App.js
+++ REQ-HTTP/httpreact/src/App.js
@@ -1,29 +1,33 @@
 import "./App.css";
 
-import { useEffect, useState } from "react";
+import { useState /* useEffect */ } from "react";
 
-const url = "http://localhost:3000/products";
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
-	const [products, setProducts] = useState([]);
-
+	/* 	const [products, setProducts] = useState([]);
+	 */
 	const [name, setName] = useState("");
 	const [price, setPrice] = useState("");
 
+	const url = "http://localhost:3000/products";
+
+	// 4 - custom
+	const { data: items, httpConfig, loading, error } = useFetch(url);
+
 	// 1 - resgatando dados
-	useEffect(() => {
-		async function fetchData() {
-			const res = await fetch(url);
+	// useEffect(() => {
+	// 	async function fetchData() {
+	// 		const res = await fetch(url);
+	// 		const data = await res.json();
 
-			const data = await res.json();
+	// 		setProducts(data);
+	// 	}
+	// 	fetchData();
+	// }, []);
 
-			setProducts(data);
-		}
-
-		fetchData();
-	}, []);
-
-	// 2 - add dados
+	// 2 - adicionando dados
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
@@ -32,43 +36,65 @@ function App() {
 			price
 		};
 
-		const res = await fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(product)
-		});
+		// 	const res = await fetch(url, {
+		// 		method: "POST",
+		// 		headers: {
+		// 			"Content-Type": "application/json"
+		// 		},
+		// 		body: JSON.stringify(product)
+		// 	});
 
-		// 2 - carregamento dinâmico
-		const addedProduct = await res.json();
+		// 	// 3 - carregamento dinâmico
+		// 	const addedProduct = await res.json();
 
-		setProducts((prevProduct) => [...prevProduct, addedProduct]);
+		// 	setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+		// 5 - refatorando POST
+		httpConfig(product, "POST");
 
 		setName("");
 		setPrice("");
+	};
+	// 8 - desafio 6
+	const handleRemove = (id) => {
+		httpConfig(id, "DELETE");
 	};
 
 	return (
 		<div className="App">
 			<h1>Lista de Produtos</h1>
-			<ul>
-				{products.map((product) => (
-					<li key={product.id}>
-						<span>{product.name}</span> <span>R${product.price}</span>
-					</li>
-				))}
-			</ul>
+			{/* 6 - loading */}
+			{loading && <p>Carregando dados...</p>}
+			{error && <p>{error}</p>}
+
+			{!loading && (
+				<ul>
+					{items &&
+						items.map((product) => (
+							<li key={product.id}>
+								{product.name} - R$ {product.price}
+								<button
+									className="button-delete"
+									onClick={() => {
+										handleRemove(product.id);
+									}}
+								>
+									Excluir
+								</button>
+							</li>
+						))}
+				</ul>
+			)}
+
 			<div className="add-product">
+				<h2>Criar Produto</h2>
+
 				<form onSubmit={handleSubmit}>
-					<h2>Criar Produto</h2>
 					<label>
 						Nome:
 						<input
 							type="text"
 							value={name}
-							name="name"
-							placeholder=""
 							onChange={(e) => setName(e.target.value)}
 						/>
 					</label>
@@ -77,11 +103,12 @@ function App() {
 						<input
 							type="number"
 							value={price}
-							name="price"
 							onChange={(e) => setPrice(e.target.value)}
 						/>
 					</label>
-					<input type="submit" value="Criar" />
+					{/* 7 - state de loading no post */}
+					{loading && <input type="submit" disabled value="Aguarde" />}
+					{!loading && <input type="submit" value="Criar" />}
 				</form>
 			</div>
 		</div>
